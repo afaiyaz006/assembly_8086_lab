8166eba005553aa5021b921c3f97703916ee84bb
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-01-22 20:29:19 +0600

    structure for 8086 assembly code

---
+++ basic_structure.asm
@@ -0,0 +1,11 @@
+.MODEL SMALL ;defines memory model of the code SMALL=code in one segment,data in one segment
+.STACK 100h  ;defining stack of 100h size
+.DATA        ;data section
+;data defination goes here
+.CODE        ;code section
+MAIN PROC    ;main procedure
+;instructions goes here
+MAIN ENDP
+;other procedure
+END MAIN     ;END directive
+
