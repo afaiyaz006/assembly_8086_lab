848b5dc31423f974e330fcfe7cb77b6ddb65d6f2
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-01-23 01:52:32 +0600

    updated comments

--- charinput_and_display.asm
+++ charinput_and_display.asm
@@ -1,18 +1,18 @@
 .MODEL SMALL
 .CODE
 MAIN PROC
-    ;showing visual promt
-    MOV AH,2
-    MOV DL,'?'
-    INT 21h 
+    ;showing visual prompt
+    MOV AH,2   ;sepcifiying function number to AH register 2 means display
+    MOV DL,'?' ;storing the char '?' to Data register which will be displayed
+    INT 21h    ;invoking BIOS routine.INT means interrupt which is an instruction to specify a BIOS routine which was specified in AH register(2 here)
     ;taking input
-    MOV AH,1
-    INT 21h
-    MOV BL,AL
+    MOV AH,1   ;again specifying function number to accumulator register 1 means taking input
+    INT 21h    ;invoking bios routine to input char(1 here specified in the AH register)
+    MOV BL,AL  ;Moving data from AL to BL register.Data we have taken input is stored to AL register
     ;displayign it
-    MOV AH,2
-    MOV DL,BL
-    INT 21h
+    MOV AH,2   ;again function number 2 means display
+    MOV DL,BL  ;moving data from BL to DL register
+    INT 21h    ;invoking BIOS routine to execute display function(2 here specified in the AH register)
 MAIN ENDP
 END MAIN
 
