9e6e2c62d0af9d8e6a70cb2bffe84056da9c7a07
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-01-23 08:35:35 +0600

    added comments

--- string_display.asm
+++ string_display.asm
@@ -1,14 +1,17 @@
 .MODEL SMALL
 .STACK 100h
 .DATA
-MSG DB 'WORKING!$'
+MSG DB 'WORKING!$'  ; Double byte message to display
 .CODE
 MAIN PROC
-   MOV AX,@DATA
+   ;initializing the data segment
+   MOV AX,@DATA  
    MOV DS,AX
-   LEA DX,MSG
-   MOV AH,9
-   INT 21h
+   ;transfering data to Data Register from MSG vairable
+   LEA DX,MSG ;LEA means Load Effective Adress it copies the offset adress of the source to destination
+   ;displaying the message
+   MOV AH,9 ;function number 9 means display string
+   INT 21h ;Starting BIOS interruption for function 9
 MAIN ENDP
 END MAIN 
     
