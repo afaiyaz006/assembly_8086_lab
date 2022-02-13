0a0fff28f53f550d2080d2fe5b3fae491828e89d
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-02-13 07:24:53 +0600

    solved

---
+++ printing_five_variables.asm
@@ -0,0 +1,83 @@
+.MODEL SMALL ;defines memory model of the code SMALL=code in one segment,data in one segment
+.STACK 100H  ;defining stack of 100h size
+.DATA        ;data section
+
+;5 variables
+NUMD DB 48
+NUMH DB 33H
+CHAR DB 'A'
+MSG  DB 'HELLO$'
+UNKNOWN DB ?
+.CODE        ;code section
+MAIN PROC    ;main procedure
+	;instructions goes here
+    ;INITIALIZING DATA SEGMENT
+    MOV AX,@DATA
+    MOV DS,AX
+        
+    ;TAKING INPUT AND STORING IT TO 'UNKNOWN'
+    MOV AH,1
+    INT 21H
+    MOV UNKNOWN,AL
+   
+    MOV AH,2
+    MOV CL,0H
+    
+    ;ADDITIONAL NEWLINE AFTER INPUT
+    MOV DL,0DH
+    INT 21H
+    MOV DL,0AH
+    INT 21H
+     
+    
+    ;PRINTING 5 VARIABLES
+PRINT_NUMD:
+    MOV DL,NUMD
+    INT 21H    
+    JMP PRINT_NEWLINE
+    
+PRINT_NUMH:
+    MOV DL,NUMH
+    INT 21H
+    JMP PRINT_NEWLINE
+
+PRINT_CHAR:
+    MOV DL,CHAR
+    INT 21H
+    JMP PRINT_NEWLINE
+
+PRINT_MSG:
+    MOV AH,9
+    MOV DX,OFFSET MSG
+    INT 21H
+    MOV AH,2
+    JMP PRINT_NEWLINE
+
+PRINT_UNKNOWN:
+    MOV DL,UNKNOWN
+    INT 21H
+    JMP PRINT_NEWLINE    
+
+PRINT_NEWLINE:
+    MOV DL,0DH
+    INT 21H
+    MOV DL,0AH
+    INT 21H
+    INC CL
+    CMP CL,1H
+    JE  PRINT_NUMH
+    CMP CL,2H
+    JE  PRINT_CHAR
+    CMP CL,3H
+    JE  PRINT_MSG
+    CMP CL,4H
+    JE  PRINT_UNKNOWN 
+	
+	MOV AH,4CH   ;4CH function code for exit
+	INT 21H	     ;exit and return control to operating system
+MAIN ENDP    ;exit main procedure	
+;other procedure
+END MAIN     ;END directive
+;end file
+
+
